842e7001e1a27c91fd32250f2f817cafd5635314
Joseph Lynch <jlynch17@binghamton.edu>
2026-03-11 19:44:38 -0400

    create log-scaled jitter/box plot for 1d data

--- workflow/viz_1d_data.py
+++ workflow/viz_1d_data.py
@@ -14,11 +14,19 @@ sns.boxplot(data=df, x="group", y="value")
 #raw obs using jitter 
 sns.stripplot(data=df, x="group", y="value", jitter=True, alpha=0.7)
 
+#(added after first rendering attempt)Log scale for improved readability
+plt.yscale("log")
+
+
+
+
 #labels/title
 plt.xlabel("group")
-plt.ylabel("value")
-plt.title("1d DATA: Cases v Control")
+plt.ylabel("value (log scale)")
+plt.title("1D DATA: Cases v Control")
 
+
+#saving
 plt.tight_layout()
 plt.savefig("figs/fig-1d-data.png", dpi=300)
 plt.close()
